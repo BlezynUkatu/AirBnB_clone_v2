8a9a5989f993ceaa3ab14ee67d197c280aa21e5e
Blessing O. Ukatu <101553471+BlezynUkatu@users.noreply.github.com>
2022-10-12 22:13:35 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
+
+time_fmt = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 class BaseModel:
@@ -13,14 +16,26 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            models.storage.new(self)
+        # assigning value to class attributes
+        # because id, updated_at and created_at
+        # are not passed as kwargs
+        if kwargs:
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = datetime.strptime(kwargs['updated_at'], time_fmt)
+                if key == 'updated_at':
+                    value = datetime.strptime(kwargs['created_at'], time_fmt)
+                if key != '__class__':
+                    setattr(self, key, value)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
